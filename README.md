84d9975b9389d4e718ed12be2f1f62073fbc3b57
hrahim-moj <118376446+hrahim-moj@users.noreply.github.com>
2023-10-19 12:11:54 +0100

    Added README, pared down front door to About and Blog. (#8)
    * added README
    
    * updated footer repo link
    
    * pared down front door
    
    * removed trailing spaces
    
    * ran standard --fix

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Data Platform User Guidance
+
+The Data Platform User Guidance is a portal intended to provide supplementary information about the [Data Platform](https://github.com/ministryofjustice/data-platform), such as how to guides, as well as comms and blogs for prospective users and anyone else interested in learning about the platform.
+
+Implemented using the [GOV.UK Eleventy Plugin](https://github.com/x-govuk/govuk-eleventy-plugin), it is a node.js-based static site generator with content written in Markdown.
+
+## Technical documentation
+
+See the [GOV.UK Eleventy Plugin documentation](https://x-govuk.github.io/govuk-eleventy-plugin/get-started/) for instructions on how to use this site's tooling.
+
+For more advanced configurations, see the [Eleventy documentation](https://www.11ty.dev/).
+
+Note that not all configurations in the Eleventy documentation are compatible with the GOV.UK Eleventy Plugin.
+
+### Before running the app (if applicable)
+
+To preview documentation locally, you require a Docker installation. Otherwise, ensure you meet the requirements stated in the [Plugin's Get Started guide](https://x-govuk.github.io/govuk-eleventy-plugin/get-started/).
+
+### Previewing the documentation
+
+The `--npx eleventy serve` command does not work with this repository as we use Docker instead for previewing documentation.
+
+Instead, once you have installed Docker, navigate to the root directory and run `bash scripts/serve.sh`.
+
+## Licence
+
+[MIT License](https://github.com/ministryofjustice/data-platform-user-guidance/blob/main/LICENSE)
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,9 @@
 const govukEleventyPlugin = require('@x-govuk/govuk-eleventy-plugin')
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Set custom variable to decide the path prefix as it is used in a couple of places.
-  const _customPathPrefix = process.env.PATH_PREFIX ?? '';
-  eleventyConfig.addPassthroughCopy({ 'docs/images': 'images' });
+  const _customPathPrefix = process.env.PATH_PREFIX ?? ''
+  eleventyConfig.addPassthroughCopy({ 'docs/images': 'images' })
   // Register the plugin
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     fontFamily: 'roboto, system-ui, sans-serif',
@@ -22,71 +21,71 @@ module.exports = function(eleventyConfig) {
     },
     footer: {
       meta: {
-          items: [
+        items: [
           {
-              href: _customPathPrefix+'/about/',
-              text: 'About'
+            href: _customPathPrefix + '/about/',
+            text: 'About'
           },
           {
-              href: _customPathPrefix+'/cookies/',
-              text: 'Cookies'
+            href: _customPathPrefix + '/cookies/',
+            text: 'Cookies'
           },
           {
-              href: 'https://github.com/ministryofjustice/data-platform',
-              text: 'GitHub'
+            href: 'https://github.com/ministryofjustice/data-platform-user-guidance',
+            text: 'GitHub'
           }
         ]
       }
-  },
-        stylesheets: ['/styles/base.css'],
-  });
-// Used for tag page generation
-eleventyConfig.addFilter("getAllTags", collection => {
-  let tagSet = new Set();
-  for(let item of collection) {
-      (item.data.tags || []).forEach(tag => tagSet.add(tag));
-  }
-  return Array.from(tagSet);
-});
-
-eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-  // Tags in array are ignored, no tag list page is generated for these
-  return (tags || []).filter(tag => ["homepage"].indexOf(tag) === -1);
-});
-
-  eleventyConfig.addCollection("homepageLinks", function(collectionApi) {
-    return collectionApi.getFilteredByGlob([
-      "**/analytical-platform.md",
-      "**/blog.md",
-      "**/data-platform.md",
-      "**/support.md",
-      "**/tech-docs.md"]).sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-  });
-
-  eleventyConfig.addCollection("getAllADRsOrderedByTitle", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("**/tech-docs/ADRs/*.md").sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-  });
-
-  eleventyConfig.addCollection("getAllBlogsOrderedByTitle", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("**/blog/*.md").sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-  });
-
-eleventyConfig.addGlobalData("phaseBannerConfiguration", () => {
-  return {
-    tag: {
-      text: "Discovery"
     },
-    html: 'This service is in the Discovery phase. Development is subject to change.'
-  }
-});
+    stylesheets: ['/styles/base.css']
+  })
+  // Used for tag page generation
+  eleventyConfig.addFilter('getAllTags', collection => {
+    const tagSet = new Set()
+    for (const item of collection) {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag))
+    }
+    return Array.from(tagSet)
+  })
 
-eleventyConfig.addGlobalData('pathPrefix', _customPathPrefix);
+  eleventyConfig.addFilter('filterTagList', function filterTagList (tags) {
+  // Tags in array are ignored, no tag list page is generated for these
+    return (tags || []).filter(tag => ['homepage'].indexOf(tag) === -1)
+  })
+
+  eleventyConfig.addCollection('homepageLinks', function (collectionApi) {
+    return collectionApi.getFilteredByGlob([
+      '**/analytical-platform.md',
+      '**/blog.md',
+      '**/data-platform.md',
+      '**/support.md',
+      '**/tech-docs.md']).sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title) // sort by title ascending
+    })
+  })
+
+  eleventyConfig.addCollection('getAllADRsOrderedByTitle', function (collectionApi) {
+    return collectionApi.getFilteredByGlob('**/tech-docs/ADRs/*.md').sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title) // sort by title ascending
+    })
+  })
+
+  eleventyConfig.addCollection('getAllBlogsOrderedByTitle', function (collectionApi) {
+    return collectionApi.getFilteredByGlob('**/blog/*.md').sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title) // sort by title ascending
+    })
+  })
+
+  eleventyConfig.addGlobalData('phaseBannerConfiguration', () => {
+    return {
+      tag: {
+        text: 'Discovery'
+      },
+      html: 'This service is in the Discovery phase. Development is subject to change.'
+    }
+  })
+
+  eleventyConfig.addGlobalData('pathPrefix', _customPathPrefix)
   return {
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
@@ -99,4 +98,4 @@ eleventyConfig.addGlobalData('pathPrefix', _customPathPrefix);
     },
     pathPrefix: _customPathPrefix
   }
-};
+}
